e47ccbe265b05735f5111b46b2c0784ed493b9ee
Jail07 <jailds07@gmail.com>
2024-12-01 19:38:26 +0600

    Finish all func and project

--- Freelance_site/settings.py
+++ Freelance_site/settings.py
@@ -134,12 +134,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static'
-# ]
-
 MEDIA_ROOT = BASE_DIR / 'media'
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Default primary key field type
